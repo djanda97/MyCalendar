71d4a295d092c212348e3c793484ed84df969448
David Janda <dvdjnd13@gmail.com>
2018-07-12 12:48:56 -0700

    Month panel appears onscreen

--- src/CalendarView.java
+++ src/CalendarView.java
@@ -3,18 +3,20 @@ import java.awt.*;
 
 public class CalendarView extends JFrame
 {
+    //private DataModel model;
     private static final int DEFAULT_WIDTH = 400;
     private static final int DEFAULT_HEIGHT = 400;
 
     public CalendarView()
     {
-        setTitle("Calendar");
-        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        JFrame frame = new JFrame();
+        frame.setTitle("Calendar");
+        frame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         MonthPanel panel = new MonthPanel();
-        add(panel);
+        frame.add(panel);
         //pack();
-        setLayout(new FlowLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        //setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
