d6e0ca1cc26d1e95ba167c903a9db236d9d1ba7e
Parnika De <parnikade@gmail.com>
2018-07-14 22:42:13 +0000

    ReadFromFile.java edited online with Bitbucket

--- src/ReadFromFile.java
+++ src/ReadFromFile.java
@@ -1,18 +1,1 @@
 
-// Java Program to illustrate reading from FileReader
-// using BufferedReader
-import java.io.*;
-public class ReadFromFile
-{
-  public static void main(String[] args)throws Exception
-  {
-  
-  File file = new File("/Users/arnabsarkar/Desktop/input.rtf");
- 
-  BufferedReader br = new BufferedReader(new FileReader(file));
- 
-  String st;
-  while ((st = br.readLine()) != null)
-    System.out.println(st);
-  }
-}
