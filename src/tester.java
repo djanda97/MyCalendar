7701e087f177be4f3d22b79d4ad585613d87b319
Parnika De <parnikade@gmail.com>
2018-07-27 05:07:35 +0000

    Working copy

---
+++ src/tester.java
@@ -0,0 +1,12 @@
+public class tester {
+    public static void main(String[] args) {
+        String date = "12:34";
+        String day = date.substring(0, 2);
+        System.out.println(day);
+        String month = date.substring(3, 5);
+        System.out.println(month);
+//        String year = date.substring(6);
+//        System.out.println(year);
+
+    }
+}
