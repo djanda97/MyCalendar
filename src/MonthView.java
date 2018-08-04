f9a56778188c0267f50d9dc2f6a21511d30e5679
Parnika De <parnikade@gmail.com>
2018-08-04 18:48:33 +0000

    Updating MonthView to fix the cell highlights

--- src/MonthView.java
+++ src/MonthView.java
@@ -68,9 +68,9 @@ public class MonthView extends JPanel implements ChangeListener, View {
         for(int i = 0; i < TIME_ROWS; ++i){
             for(int j = 0; j < daysInMonth; ++j){
             	if(i == 0) {
-            		eventTableModel.setValueAt("" + j, i, j);
+            		eventTableModel.setValueAt(j + "", i, j);
             	} else {
-            		eventTableModel.setValueAt("" + 0, i, j);
+            		eventTableModel.setValueAt("", i, j);
             	}
             }
         }
@@ -79,31 +79,42 @@ public class MonthView extends JPanel implements ChangeListener, View {
             public Component prepareRenderer(TableCellRenderer renderer, int row, int column)
             {
                 Component c = super.prepareRenderer(renderer, row, column);
-                	System.out.println(column);
+
                     if(!isRowSelected(row)){
                         c.setBackground(getBackground());
-                        int modelRow = convertRowIndexToModel(row);
-                        String hiddenData = (String)getModel().getValueAt(modelRow, COLUMNS-2);
-                        String[] data = hiddenData.split("-");
-                        if("1".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.YELLOW);
-                        if("2".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(176,224,230));
-                        if("3".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.RED);
-                        if("4".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.PINK);
-                        if("5".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.ORANGE);
-                        if("6".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.MAGENTA);
-                        if("7".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.LIGHT_GRAY);
-                        if("8".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(Color.GREEN);
-                        if("9".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(128, 0, 128));
-                        if("10".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(0, 128, 128));
-                        if("11".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(152,251,152));
-                        if("12".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(128, 128, 0));
-                        if("13".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(128, 0, 0));
-                        if("14".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(192, 192, 192));
-                        if("15".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(0, 255, 255));
-                        if("16".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(255, 215, 0));
-                        if("17".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(255, 127, 0));
-                        if("18".equals(data[0]) && column == Integer.parseInt(data[1])) c.setBackground(new Color(210, 105, 30));
-
+                        //int modelRow = convertRowIndexToModel(row);
+                        String hiddenDataString = (String)getModel().getValueAt(0, COLUMNS-2);
+                        //System.out.println(hiddenDataString);
+                        String[] hiddenDataRows = hiddenDataString.split("-");
+                        String[][] hiddenDataColumns = new String[TIME_ROWS][MONTH_COLUMNS];
+                        for(int i = 0; i < TIME_ROWS; i++) {
+                        	hiddenDataColumns[i] = hiddenDataRows[i].split(":");
+                        }
+                        for(int i = 0; i < TIME_ROWS; i++) {
+                        	for(int j = 0; j < MONTH_COLUMNS; j++) {
+                        		if(!"0".equals(hiddenDataColumns[i][j]) && i == row && j == column) {
+                        			int data = Integer.parseInt(hiddenDataColumns[i][j]) % 18;
+                        			if(data == 1) c.setBackground(Color.YELLOW);
+        	                        if(data == 2) c.setBackground(new Color(176,224,230));
+        	                        if(data == 3) c.setBackground(Color.RED);
+        	                        if(data == 4) c.setBackground(Color.PINK);
+        	                        if(data == 5) c.setBackground(Color.ORANGE);
+        	                        if(data == 6) c.setBackground(Color.MAGENTA);
+        	                        if(data == 7) c.setBackground(Color.LIGHT_GRAY);
+        	                        if(data == 8) c.setBackground(Color.GREEN);
+        	                        if(data == 9) c.setBackground(new Color(128, 0, 128));
+        	                        if(data == 10) c.setBackground(new Color(0, 128, 128));
+        	                        if(data == 11) c.setBackground(new Color(152,251,152));
+        	                        if(data == 12) c.setBackground(new Color(128, 128, 0));
+        	                        if(data == 13) c.setBackground(new Color(128, 0, 0));
+        	                        if(data == 14) c.setBackground(new Color(192, 192, 192));
+        	                        if(data == 15) c.setBackground(new Color(0, 255, 255));
+        	                        if(data == 16) c.setBackground(new Color(255, 215, 0));
+        	                        if(data == 17) c.setBackground(new Color(255, 127, 0));
+        	                        if(data == 0) c.setBackground(new Color(210, 105, 30));
+                        		}
+                        	}
+                        }
                     }
 
                 return c;
@@ -145,18 +156,36 @@ public class MonthView extends JPanel implements ChangeListener, View {
     @Override
     public void updateEventTable(){
         List<Event> eventList = model.getEventInSelectedView("month");
-        int hiddenData = 1;
+        int eventCount = 1;
+        int hiddenDataRow = 1;
+        int hiddenDataColumn = 1;
+        int[][] hiddenData = new int[TIME_ROWS][MONTH_COLUMNS];
+        for(int i = 0; i < TIME_ROWS; i++) {
+        	for(int j = 0; j < MONTH_COLUMNS; j++) {
+        		hiddenData[i][j] = 0;
+        	}
+        }
         for(Event event : eventList){
             int eventIndex = (int) event.getStartHour();
             int hightlightIndex = (int) event.getEndHour();
             int eventColumn = event.getDay() - 1;
             eventTableModel.setValueAt(event.getName(), eventIndex, eventColumn + COLUMNS - 1);
-            for(int i = eventIndex; i <= hightlightIndex; ++i){
-                eventTableModel.setValueAt(hiddenData + "-" + (eventColumn + COLUMNS - 1), i, COLUMNS - 2);
+            hiddenDataColumn = eventColumn + COLUMNS - 1;
+            for(hiddenDataRow = eventIndex; hiddenDataRow < hightlightIndex; hiddenDataRow++){
+            	hiddenData[hiddenDataRow][hiddenDataColumn] = eventCount;
             }
-            ++hiddenData;
+            eventCount++;
+            
         }
-
+        String hiddenDataString = "";
+        for(int i = 0; i < TIME_ROWS; i++) {
+        	for(int j = 0; j < MONTH_COLUMNS; j++) {
+        		hiddenDataString += hiddenData[i][j] + ":";
+        	}
+        	hiddenDataString += "-";
+        	//System.out.println(hiddenDataString);
+        }
+        eventTableModel.setValueAt(hiddenDataString, 0, COLUMNS - 2);
         for(int i = 0; i < TIME_ROWS; ++i){
             eventTable.setRowHeight(i, ROW_HEIGHT);
         }
