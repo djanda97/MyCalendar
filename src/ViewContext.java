7aae8719033acf636478be897054543a8ea010e3
Parnika De <parnikade@gmail.com>
2018-08-04 23:54:45 +0000

    ViewContext.java created online with Bitbucket

---
+++ src/ViewContext.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Context class
+ *
+ */
+public class ViewContext {
+	View view;
+	
+	public ViewContext(View view) {
+		this.view = view;
+	}
+
+	public View getView() {
+		return view;
+	}
+
+	public void setView(View view) {
+		this.view = view;
+	}
+}
